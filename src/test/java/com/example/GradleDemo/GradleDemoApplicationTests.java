06ef171d5f2f42948839b602eab7612666e25f04
manjunathabn1 <manjunatha.b.n@volkswagen.co.in>
2022-02-02 16:54:26 +0530

    first commit

---
+++ src/test/java/com/example/GradleDemo/GradleDemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.GradleDemo;
+
+import org.junit.jupiter.api.*;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("888888888888");
+	}
+
+}
